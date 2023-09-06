ac0190ef88db67e2d509c223a9d7f69d8b75b336
Antônio Pedro <113259985+Tun1n@users.noreply.github.com>
2023-09-06 16:40:25 -0300

    Exercícios gallo

--- Exercício 10/main.js
+++ Exercício 10/main.js
@@ -3,7 +3,9 @@ button.addEventListener('click', () => {
 
     let idade = Number(document.getElementById("num1").value)
     let h2 = document.querySelector("h2")
-    if (idade >=5 && idade <= 7)
+    if (idade < 5)
+        h2.innerHTML = `Ele não é nadador`
+    else if (idade >=5 && idade <= 7)
         h2.innerHTML = `O nadador está no infantil A`
     else if (idade >= 8 && idade<=10)
         h2.innerHTML = `O nadador está no infantil B`
